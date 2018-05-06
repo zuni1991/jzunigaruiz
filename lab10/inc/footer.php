a6d29b1a3789262bcef34db13e9e46bc735acf54
Juan Zuniga <jzunigaruiz@csumb.edu>
2018-05-06 05:35:50 +0000

    final

--- lab10/inc/footer.php
+++ lab10/inc/footer.php
@@ -1,3 +1,5 @@
         <hr>
         <strong>Disclaimer:</strong> All information in this website is fake. <br />
         &copy;2018 Juan Zuniga
+        <br /><br />
+        <img id="logo" src="img/ver.png" alt="CSUMB Logo"/>
